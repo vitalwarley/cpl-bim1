8273e49714a5c5774973996afc99405ac9a44114
vitalwarley <wvb@ic.ufal.br>
2018-05-09 21:07:16 -0300

    add code to read grammar from file; add code to break gramar into Non-terminals and its RHS.

--- src/main/java/br/ufal/ic/parser/Parser.java
+++ src/main/java/br/ufal/ic/parser/Parser.java
@@ -1,34 +1,59 @@
 package br.ufal.ic.parser;
 
 import br.ufal.ic.lexer.Application;
-import br.ufal.ic.lexer.Lexer;
-import br.ufal.ic.lexer.Token;
 import javafx.util.Pair;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Parser {
 
     private Application application;
     private static Stack<String> stack = new Stack<>();
     private static Hashtable<Pair<String, String>, List<String>> parsingTable = new Hashtable<>();
-    private static final List<String> nonTerminals = Arrays.asList("E", "E'", "T", "T'", "F");
+
+    //    private static List<String> nonTerminals = Arrays.asList("E", "E'", "T", "T'", "F");
+    private static Set<String> nonTerminals;
+    private static Map<String, List<String>> productions = new HashMap<>();
+    private static List<String> grammar;
+    private static HashMap<String, List<String>> first;
+    private static HashMap<String, List<String>> followt;
 
 
     public static void main(String[] args) {
-        initParsingTable();
+        /*initParsingTableTest();
         String[] input = {"id", "+", "id", "*", "id", "$"};
         try {
             predictiveParsing(input);
         } catch (EmptyStackException e) {
             System.err.println("erro: stack vazia");
         }
+        */
+        initGrammar("grammar_ll1.txt");
     }
 
+    private static void initGrammar(String path) {
+        try (Stream<String> stream = Files.lines(Paths.get(path))) {
+            grammar = stream.collect(Collectors.toList());
+
+            nonTerminals = grammar.stream().
+                    map(s -> {
+                        addProduction(s);
+                        return s.split(" -> ")[0];
+                    }).
+                    collect(Collectors.toSet());
+
+        } catch (IOException e) {
+            System.err.println("Não foi possível ler o arquivo que contém a gramática.");
+        }
+    }
 
     @SuppressWarnings("unchecked")
-    private static void initParsingTable() {
+    private static void initParsingTableTest() {
         String epsilon = "\uD835\uDEDC";
         parsingTable.put(new Pair("E", "id"), Arrays.asList("T", "E'"));
         parsingTable.put(new Pair("E", "("), Arrays.asList("T", "E'"));
@@ -44,6 +69,7 @@ public class Parser {
         parsingTable.put(new Pair("F", "id"), Arrays.asList("id"));
         parsingTable.put(new Pair("F", "("), Arrays.asList("(", "E", ")"));
     }
+
 
     @SuppressWarnings("unchecked")
     public static void predictiveParsing(String[] input) throws EmptyStackException {
@@ -104,4 +130,12 @@ public class Parser {
         return productionReversed;
     }
 
+
+    private static void addProduction(String rule) {
+        String[] parts = rule.split(" -> ");
+        if (productions.get(parts[0]) == null)
+            productions.put(parts[0], new ArrayList<>(Arrays.asList(parts[1])));
+        else
+            productions.get(parts[0]).add(parts[1]);
+    }
 }
